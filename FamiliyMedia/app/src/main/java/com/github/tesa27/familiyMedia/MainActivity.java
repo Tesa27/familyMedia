b8f14559c6a9ffad5c9d66ae5716eb7ad72a623c
Tesa27 <stefan.eric@epfl.ch>
2023-02-08 19:29:28 +0100

    only login fragment setup

--- FamiliyMedia/app/src/main/java/com/github/tesa27/familiyMedia/MainActivity.java
+++ FamiliyMedia/app/src/main/java/com/github/tesa27/familiyMedia/MainActivity.java
@@ -1,8 +1,11 @@
 package com.github.tesa27.familiyMedia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import com.github.tesa27.familiyMedia.ui.login.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment fragment = new LoginFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container_login, fragment)
+                .commit();
+
     }
 }
